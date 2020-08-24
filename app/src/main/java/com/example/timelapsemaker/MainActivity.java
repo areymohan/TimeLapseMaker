cef846d6f188fbc6f747ee316b47387f97b77a51
Harshini Swaminathan <harshini098@gmail.com>
2020-08-24 19:57:49 +0530

    Commit 2

--- app/src/main/java/com/example/timelapsemaker/MainActivity.java
+++ app/src/main/java/com/example/timelapsemaker/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.timelapsemaker;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -12,35 +19,78 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    Button button;
-    private static final int PICK_IMAGE = 100;
-    Uri imageUri;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView)findViewById(R.id.imgView);
-        button = (Button)findViewById(R.id.buttonLoadPicture);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        Button b=findViewById(R.id.imgbutton);
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openGallery();
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},100);
+                    return ;
+                }
+                Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+                intent.setType("image/+");
+                startActivityForResult(intent,1);
             }
         });
     }
-    private void openGallery() {
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(gallery, PICK_IMAGE);
-    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
-            imageUri = data.getData();
-            imageView.setImageURI(imageUri);
+        if(requestCode==1&&resultCode==RESULT_OK){
+            final ImageView imgView=findViewById(R.id.imgView);
+            final List<Bitmap> bitmaps=new ArrayList<>();
+            ClipData clipData=data.getClipData();
+            if(clipData!=null){
+                for(int i=0;i<clipData.getItemCount();i++){
+                    Uri imageUri=clipData.getItemAt(i).getUri();
+                    try{
+                        InputStream is=getContentResolver().openInputStream(imageUri);
+                        Bitmap bitmap= BitmapFactory.decodeStream(is);
+                        bitmaps.add(bitmap);
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+            }else{
+                Uri imageUri=data.getData();
+                try {
+                    InputStream is=getContentResolver().openInputStream(imageUri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(final Bitmap b:bitmaps){
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imgView.setImageBitmap(b);
+                            }
+                        });
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
         }
     }
 }
